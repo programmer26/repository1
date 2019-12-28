bd446d763061e97ce613367f7e1c0bd1bf889076
Admin <denislav202@gmail.com>
2019-12-28 21:32:12 +0200

    Added console engine

---
+++ Console Graphics Engine/cge.hpp
@@ -0,0 +1,9 @@
+#ifndef CGE_HPP
+#define CGE_HPP
+
+int SET_UP(std::string mode);
+void fill(int width, int height, char input);
+void draw_square(int width, int height, unsigned char top, unsigned char sides);
+void draw_triangle(int width, int height, unsigned char chr);
+
+#endif
